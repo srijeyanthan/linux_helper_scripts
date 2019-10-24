80772c11e55a228557236566b5dec9aaafbd5657
srijeyanthan <srijeyanthan@gmail.com>
2019-10-24 18:09:28 +0800

    added complex string generations
    added function to generate for mac users

--- random_string.sh
+++ random_string.sh
@@ -2,11 +2,22 @@
 # Author : Sri
 
 ## inputs are,  number of times
-random_string()
+random_string_unix()
 {
     cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-30} | head -n 1
 }
 
+### for mac users, use this
+random_string_mac()
+{
+  cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 30 | head -n 1
+}
+
+### sometime, if we need extra characters like this !@#$%^&*()_+?><~`';][
+random_complex_string()
+{
+  cat /dev/urandom | tr -dc "a-zA-Z0-9!@#$%^&*()_+?><~\`;'" | fold -w 32 | head -n 1
+}
 i="0"
 while true
 do
